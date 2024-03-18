73eff2a628de95794f10f557666110a290c638bb
MoistyRain <111257933+MoistyRain@users.noreply.github.com>
2024-03-18 18:21:02 -0500

    new custom user model, overwrote prior migrations

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models.functions import Lower
+from  django.contrib.auth.models import AbstractUser
 
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    """Model representing an online post"""
+    title =  models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
 # Create your models here.
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -143,3 +143,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use a custom user model
+AUTH_USER_MODEL =  "blog.CustomUser"
