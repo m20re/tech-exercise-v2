4b4e02d9c02acb515cf09b9cdef953d3108e7ed3
MoistyRain <111257933+MoistyRain@users.noreply.github.com>
2024-03-19 15:36:49 -0500

    Added homepage, along with css and html templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Post, CustomUser
 # Create your views here.
+
+def index(request):
+    """View function for website homepage"""
+
+    # Prints the amount of posts
+    num_posts = Post.objects.all().count()
+
+    # Prints the amount of users
+    num_users = CustomUser.objects.all().count()
+
+    context = {
+        'num_users' : num_users,
+        'num_posts' : num_posts,
+    }
+    
+    # Render the HTML template index.html with information within context
+    return render(request, 'index.html', context=context)
