a81f588bebf3cdcd70dc4675d2e389581df65019
MoistyRain <111257933+MoistyRain@users.noreply.github.com>
2024-03-21 23:39:28 -0500

    added more functionality to accounts app

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blog.forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Create your views here.
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    # if the request is passing information, change the logic
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        # if the form information is valid, save it to the DB
+        if form.is_valid():
+            form.save()
+            # log the user in
+            return redirect('blog:posts')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/signup.html', {'form':form})
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,8 @@
 from  django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/', views.PostListView.as_view(), name='posts'),
